aa0e772de6b58f414d7091acbd52b2ad50ce29a7
Raquel Leal <164494185+LealRaquel@users.noreply.github.com>
2025-09-18 16:02:45 +0100

    Add files via upload

---
+++ primeiro_projecto/src/Primeiro_Projecto.java
@@ -0,0 +1,19 @@
+public class Primeiro_Projecto {
+    public static void main(String[]args) {
+        String nome = "Raquel";
+        String morada = "F.Foz";
+        int idade = 18;
+        char escivil='s';
+        double nota1= 15.0;
+        double nota2= 10.5;
+        double media = (nota1+nota2)/2;
+
+        System.out.println("Nome:" + nome);
+        System.out.println("Idade:" + idade);
+        System.out.println("Escivil:" + escivil);
+        System.out.println("Morada:" + morada);
+        System.out.println("Media:"+ media);
+
+        System.out.println("Nome:" + nome + ", Morada:" + morada + ", Idade:" + idade + ", Escivil:" + escivil + ", Media:" + media);
+    }
+}
