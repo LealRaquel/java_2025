c9a620dc9878e6f23814d90d5861941ee7b5308c
Raquel Leal <164494185+LealRaquel@users.noreply.github.com>
2025-09-23 15:06:05 +0100

    Add files via upload

---
+++ Actividade3/src/Acividade3.java
@@ -0,0 +1,12 @@
+public class Acividade3 {
+    public static void main (String[] args){
+        int valor = 30;
+
+        if(valor<= 50){
+            System.out.println("O livro está barrato!!");
+
+        }else {
+            System.out.println("O livro está muito carro!!!");
+        }
+    }
+}
