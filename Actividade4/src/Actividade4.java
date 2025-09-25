688f498f9228cca196f2d0c6ec057f13997f4044
Raquel Leal <164494185+LealRaquel@users.noreply.github.com>
2025-09-25 15:17:23 +0100

    Add files via upload

---
+++ Actividade4/src/Actividade4.java
@@ -0,0 +1,26 @@
+public class Actividade4 {
+    public static void main(String[]arg){
+        char consoante= 'R';
+
+        switch(consoante){
+            case 'a':
+                System.out.println("É uma vogal");
+                break;
+            case 'e':
+                System.out.println("Também é uma vogal");
+                break;
+            case 'i':
+                System.out.println(" É vogal");
+                break;
+            case 'o':
+                System.out.println("Também é vogal");
+                break;
+            case 'u':
+                System.out.println("Também é vogal");
+                break;
+            default:
+                System.out.println("É uma consoante!!");
+                break;
+        }
+    }
+}
