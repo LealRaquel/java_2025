0ada5ff87bcd0a308e66a87b3d22f1761d086422
Raquel Leal <164494185+LealRaquel@users.noreply.github.com>
2025-10-10 09:25:31 +0100

    Add files via upload

---
+++ Atividade13/src/Atividade13.java
@@ -0,0 +1,11 @@
+public class Atividade13 {
+    public static  void main(String[]args){
+        int agua =100;
+
+        if (agua>=100){
+            System.out.println("A água está ferver!");
+        }else {
+            System.out.println("A água não está a ferver!");
+        }
+    }
+}
