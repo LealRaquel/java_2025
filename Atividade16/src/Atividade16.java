cc27156338f595f29ad878a85a7f2c6be215ef51
Raquel Leal <164494185+LealRaquel@users.noreply.github.com>
2025-10-31 09:28:40 +0000

    Add files via upload

---
+++ Atividade16/src/Atividade16.java
@@ -0,0 +1,15 @@
+public class Atividade16 {
+    public static void main (String[] args){
+        String [] convidados= new String[7];
+
+        convidados = new String[] {"Ana", "Olívia", "Óscar", "Maria", "Francisco", "Pedro", "Miguel"};
+
+        System.out.println("Os convidados da festa são: ");
+
+        for( int i = 0; i< convidados.length; i ++){
+            System.out.println( convidados[i]);
+        }
+
+
+    }
+}
