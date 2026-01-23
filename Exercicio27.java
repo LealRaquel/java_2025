e9afd0d412e3c97e8ae14b3198eeef7f30bfe564
Raquel Leal <164494185+LealRaquel@users.noreply.github.com>
2026-01-23 15:28:59 +0000

    Add files via upload

---
+++ Exercicio27.java
@@ -0,0 +1,15 @@
+public class Exercicio27 {
+    public static void main(String[] args) {
+        String idade = "18";
+
+        //Converter a string para o int
+        int idade2 = Integer.parseInt(idade);
+        System.out.println("Valor Int: " + idade2);
+
+        //Converter o int para o float
+        float idade3 = Float.parseFloat(idade);
+        System.out.println("valor Float: " + idade3);
+
+    }
+    
+}
